b83ff7ee3ee810489c854cf2235d907d8750a226
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-04-15 00:38:44 +0200

    third commit

--- controller/main_controller.js
+++ controller/main_controller.js
@@ -27,20 +27,38 @@ exports.postChange = async (req, res) => {
     
     let newScore = (req.body.scoreOverwrite) ? req.body.scoreOverwrite.toString() : null;
     let nameChangeScore = (req.body.nameChangeScore) ? req.body.nameChangeScore.toString() : null;
-    // let nameHundred = (req.body.addHundredScore) ? req.body.addHundredScore.toString() : null;
+    let deleteUser = (req.body.deleteUser) ? req.body.deleteUser.toString() : null;
+    let insertUserName = (req.body.insertUserName) ? req.body.insertUserName.toString() : null;
+    let insertUserScore = (req.body.insertUserScore) ? parseInt(req.body.insertUserScore) : null;
     
-    let post = {
-            username : 'Pawel',
-            score : newScore
-        };
+    let userIdUpdate, sqlQuery;
+
+    if (insertUserName && insertUserScore) {
+
+        let insertObject = [insertUserName, insertUserScore];
+
+        sqlQuery = `INSERT INTO user_score (username, score) VALUES (?,?)`;
+        con.query(sqlQuery, insertObject, (err, res) =>{
+            if (err) throw err;
+            console.log(`New user: ${insertUserName}, with score: ${insertUserScore}`);
+            });
+    }
 
     if (newScore && nameChangeScore) {
-        let userIdUpdate = nameChangeScore.charAt(0);
-        let sqlQuery = `UPDATE user_score SET score = ${newScore} WHERE user_id = ${userIdUpdate}`;
+        userIdUpdate = nameChangeScore.substr(0, nameChangeScore.indexOf('.'));
+        sqlQuery = `UPDATE user_score SET score = ${newScore} WHERE user_id = ${userIdUpdate}`;
         con.query(sqlQuery, (err, res) =>{
             console.log(`${newScore} set for ${nameChangeScore}`);
             });
-        }
+    }
+
+    if (deleteUser) {
+        userIdUpdate = deleteUser.substr(0, deleteUser.indexOf('.'));
+        sqlQuery = `DELETE FROM user_score WHERE user_id = ${userIdUpdate}`;
+        con.query(sqlQuery, (err, res) => {
+            console.log(`${deleteUser} deleted`)
+        })
+    }
     
         res.render('change' );
 
